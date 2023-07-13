f44224e0b2880f52a1e46d3df8015bf3df2fd015
EC2 Default User <ec2-user@ip-10-240-0-182.ec2.internal>
2023-07-13 07:28:23 +0000

    Added Manifests Directory

--- bin/cdk-eks.ts
+++ bin/cdk-eks.ts
@@ -1,21 +1,62 @@
-#!/usr/bin/env node
+import { Construct } from 'constructs';
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { CdkEksStack } from '../lib/cdk-eks-stack';
+import * as eks from 'aws-cdk-lib/aws-eks';
+import * as blueprints from '@aws-quickstart/eks-blueprints';
+import * as ec2 from 'aws-cdk-lib/aws-ec2';
 
 const app = new cdk.App();
-new CdkEksStack(app, 'CdkEksStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const addOns: Array<blueprints.ClusterAddOn> = [
+	    new blueprints.addons.EbsCsiDriverAddOn(),
+	    new blueprints.addons.VpcCniAddOn(),
+	    new blueprints.addons.CoreDnsAddOn('v1.9.3-eksbuild.5'),
+	    new blueprints.addons.KubeProxyAddOn('v1.26.6-eksbuild.1'),
+	    new blueprints.addons.AwsLoadBalancerControllerAddOn(),
+	    new blueprints.addons.ArgoCDAddOn({
+                bootstrapRepo: {
+                    repoUrl: 'https://github.com/sathish2304/eks-blueprints-repo.git',
+                    path: 'manifests/StorageClass',
+                     targetRevision: "main",
+                     },})
+];
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+const clusterProvider = new blueprints.GenericClusterProvider({
+    version: eks.KubernetesVersion.V1_26,
+    tags: {
+        "Name": "backup-example-cluster",
+        "Type": "generic-cluster"
+    },
+    managedNodeGroups: [
+        {
+            id: "mng1",
+            amiType: eks.NodegroupAmiType.AL2_X86_64,
+            instanceTypes: [new ec2.InstanceType('m5.2xlarge')],
+            desiredSize: 2,
+            maxSize: 3, 
+            nodeGroupSubnets: { subnetType: ec2.SubnetType.PRIVATE_WITH_EGRESS },
+            launchTemplate: {
+                // You can pass Custom Tags to Launch Templates which gets propagated to worker nodes.
+                tags: {
+                    "Name": "Mng1",
+                    "Type": "Managed-Node-Group",
+                    "AppName": "TestApp",
+                    "Instance": "ONDEMAND"
+                }
+            }
+        }
+        
+    ]
 });
+
+
+const account = '370926141865';
+const region  = 'us-west-1';
+
+const stack = blueprints.EksBlueprint.builder()
+    .account(account)
+    .clusterProvider(clusterProvider)
+    .region(region)
+    .addOns(...addOns)
+    .build(app, 'eks-blueprint');
