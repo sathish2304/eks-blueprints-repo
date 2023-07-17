11c9d1cd5522bdc0df52401d29559639a7ec2de3
EC2 Default User <ec2-user@ip-10-240-0-182.ec2.internal>
2023-07-17 14:59:14 +0000

    Updated Storage Class Tags

--- bin/cdk-eks.ts
+++ bin/cdk-eks.ts
@@ -4,17 +4,20 @@ import * as cdk from 'aws-cdk-lib';
 import * as eks from 'aws-cdk-lib/aws-eks';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as backup from 'aws-cdk-lib/aws-backup';
+import { Duration } from 'aws-cdk-lib';
+
 
 const app = new cdk.App();
 
 const bootstrapRepo: blueprints.ApplicationRepository = {
-    repoUrl: 'https://github.com/sathish2304/eks-blueprints-repo.git',
-    targetRevision: "main"
+    repoUrl: 'https://github.com/sathish2304/eks-blueprints-repo'
 }
 
 
 const addOns: Array<blueprints.ClusterAddOn> = [
 	    new blueprints.addons.EbsCsiDriverAddOn(),
+	    new blueprints.addons.EfsCsiDriverAddOn(),
 	    new blueprints.addons.VpcCniAddOn(),
 	    new blueprints.addons.CoreDnsAddOn('v1.9.3-eksbuild.5'),
 	    new blueprints.addons.KubeProxyAddOn('v1.26.6-eksbuild.1'),
@@ -22,7 +25,7 @@ const addOns: Array<blueprints.ClusterAddOn> = [
 	    new blueprints.addons.ArgoCDAddOn({
                 bootstrapRepo: {
                     ...bootstrapRepo,
-                    path: 'manifests/StorageClass',
+                    path: './manifests/StorageClass',
                      },})
 ];
 
@@ -43,7 +46,7 @@ const clusterProvider = new blueprints.GenericClusterProvider({
             launchTemplate: {
                 // You can pass Custom Tags to Launch Templates which gets propagated to worker nodes.
                 tags: {
-                    "Name": "Mng1",
+                    "EKSPVBackup": "true",
                     "Type": "Managed-Node-Group",
                     "AppName": "TestApp",
                     "Instance": "ONDEMAND"
@@ -54,7 +57,6 @@ const clusterProvider = new blueprints.GenericClusterProvider({
     ]
 });
 
-
 const account = '370926141865';
 const region  = 'us-west-1';
 
@@ -64,3 +66,15 @@ const stack = blueprints.EksBlueprint.builder()
     .region(region)
     .addOns(...addOns)
     .build(app, 'eks-blueprint');
+    
+// Create a AWS Backup Vault
+const backupVault = new backup.BackupVault(stack, 'BackupVault', {backupVaultName: 'EKSBackupVault'});
+
+// Create a AWS Backup Backup plan to backup resources based on Tags
+const backupPlan = new backup.BackupPlan(stack, 'BackupPlan', {backupPlanName: 'EKSBackupPlan'});
+backupPlan.addRule(backup.BackupPlanRule.daily(backupVault));
+backupPlan.addSelection('EKSResources', {
+  resources: [
+    backup.BackupResource.fromTag('EKSPVBackup', 'true'), 
+  ]
+})
